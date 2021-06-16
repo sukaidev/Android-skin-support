c98b72a0e6c02e77b4086c2d06c4ed338bb29116
donglua <flyntcat@live.cn>
2021-06-16 17:21:37 +0800

    更新AppCompat

--- androidx/skin-support-appcompat/src/main/java/skin/support/content/res/SkinCompatDrawableManager.java
+++ androidx/skin-support-appcompat/src/main/java/skin/support/content/res/SkinCompatDrawableManager.java
@@ -109,13 +109,7 @@ final class SkinCompatDrawableManager {
             R.drawable.abc_textfield_activated_mtrl_alpha,
             R.drawable.abc_textfield_search_activated_mtrl_alpha,
             R.drawable.abc_cab_background_top_mtrl_alpha,
-            R.drawable.abc_text_cursor_material,
-            R.drawable.abc_text_select_handle_left_mtrl_dark,
-            R.drawable.abc_text_select_handle_middle_mtrl_dark,
-            R.drawable.abc_text_select_handle_right_mtrl_dark,
-            R.drawable.abc_text_select_handle_left_mtrl_light,
-            R.drawable.abc_text_select_handle_middle_mtrl_light,
-            R.drawable.abc_text_select_handle_right_mtrl_light
+            R.drawable.abc_text_cursor_material
     };
 
     /**
